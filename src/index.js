580e15dcfd4024435de282302d458f5770280dfa
Dominique Klevinghaus <79855242+Klekum@users.noreply.github.com>
2021-12-09 14:27:18 +0100

    Add button styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './fonts/roboto.css'
 import './global.scss'
 import './tables.scss'
-import "./buttons.scss";
+import './buttons.scss'
 
-console.log('ok')
+function buildButtonColors() {
+  const colors = ['grassgreen', 'orange', 'gold', 'sel-red', 'sel-light-gray', 'sel-gray', 'sel-dark-gray', 'sel-distinct']
+  const target = document.getElementById('button-colors')
+  colors.forEach(btnColor => {
+    const buttonLayer = document.createElement('tr')
+    buttonLayer.innerHTML = `<td>${btnColor}</td><td><button class="button-lg ${btnColor}">${btnColor}</button></td><td><button class="button-lg ${btnColor}-invers">${btnColor}-invers</button></td>`
+    target.appendChild(buttonLayer)
+  })
+}
+
+window.onload = buildButtonColors
